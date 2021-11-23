a231f8bd6605c313ad9a8f983f82c73b92b383d9
Huynh Minh Hai <67136091+HminhHai@users.noreply.github.com>
2021-11-23 17:41:28 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express'),
+	app = express(),
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	config = require('./config/config.js'),
+	mongojs = require('mongojs'),
+	db = mongojs('chatCAT', ['chatCAT']),
+	passport = require('passport'),
+	FacebookStrategy = require('passport-facebook').Strategy;
+	require('./auth/passportAuth.js')(passport, FacebookStrategy, config, db, mongojs);
+
+	app.use(passport.initialize());
+	app.use(passport.session());
+	// var userSchema = mongoose.Schema({
+	// 	username:String,
+	// 	password: String,
+	// 	fullname: String
+	// });
+	// var Person = mongoose.model('user', userSchema);
+	// var Ali = new Person({ 		
+	// 	username:"Ali",
+	// 	password: "123",
+	// 	fullname: "Alireza Saberi" });
+	// Ali.save(function(error){
+	// 	console.log("done");
+	// });
+
+
+// set the view emgine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+var env = process.env.NODE_ENV || 'development';
+	if(env === 'development'){
+		// dev specific settings
+		app.use(session({secret:config.sessionSecret}));
+		//app.use(session({secret:'AliSaberi'}));
+	} else {
+		// production specific settings
+		//app.use(session({}));
+		app.use(session({
+			secret:config.sessionSecret,
+			store: new connect-mongo({
+				url: config.dbURL,
+				stringify: true
+			})
+		}));
+	}
+
+require('./routes/route.js')(express, app, passport);
+
+app.listen(3000, function(){
+	console.log('chatCAT is running on port 3000. Press ctrl+C to exit');
+	console.log('Mode is ' + env);
+});
